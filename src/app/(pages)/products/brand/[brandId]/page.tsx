d96b4dc8fa26354455db7f91cfadbe4a2a17b479
MohamedAlaa9758 <ma2612557@gmail.com>
2026-03-28 19:44:14 +0200

    fix async params

--- src/app/(pages)/products/brand/[brandId]/page.tsx
+++ src/app/(pages)/products/brand/[brandId]/page.tsx
@@ -1,14 +1,31 @@
 
+// import CardDeisgin from '@/components/CardDeisgin/CardDeisgin';
+// import { ProductI } from '@/interfaces';
+// import { Params } from 'next/dist/server/request/params';
+
+
+// export default async function BrandProduct({ params }: { params: Params }) {
+//     const { brandId } = params;
+//     const response = await fetch('https://ecommerce.routemisr.com/api/v1/products?brand=' + brandId)
+//     const { data: products }: { data: ProductI[] } = await response.json();
+
+
+//     return <>
+//         <h1 className='text-center capitalize'>All products in the brand</h1>
+//         <div className="flex flex-wrap my-4">
+//             {products.map((product) => (
+//                 <CardDeisgin product={product} key={product.id} />
+//             ))}
+//         </div>
+//     </>
+// }
 import CardDeisgin from '@/components/CardDeisgin/CardDeisgin';
 import { ProductI } from '@/interfaces';
-import { Params } from 'next/dist/server/request/params';
 
-
-export default async function BrandProduct({ params }: { params: Params }) {
-    const { brandId } = params;
-    const response = await fetch('https://ecommerce.routemisr.com/api/v1/products?brand=' + brandId)
+export default async function BrandProduct({ params }: { params: Promise<{ brandId: string }> }) {
+    const { brandId } = await params;
+    const response = await fetch('https://ecommerce.routemisr.com/api/v1/products?brand=' + brandId);
     const { data: products }: { data: ProductI[] } = await response.json();
-
 
     return <>
         <h1 className='text-center capitalize'>All products in the brand</h1>
@@ -17,5 +34,5 @@ export default async function BrandProduct({ params }: { params: Params }) {
                 <CardDeisgin product={product} key={product.id} />
             ))}
         </div>
-    </>
+    </>;
 }
