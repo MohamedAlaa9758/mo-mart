644af69425a2b7d4a8bedf9f55ee6b6b25fc9191
MohamedAlaa9758 <ma2612557@gmail.com>
2026-03-28 19:32:45 +0200

    fix token cookie

--- src/components/Helpers/getUserToken.ts
+++ src/components/Helpers/getUserToken.ts
@@ -1,20 +1,39 @@
+// // import { decode } from "next-auth/jwt";
+// // import { cookies } from "next/headers";
+
+// // export async function getUserToken() {
+// //   const x = (await cookies()).get("next-auth.session-token")?.value;
+// //   const accessToken = await decode({ token: x, secret: process.env.NEXTAUTH_SECRET! });
+// //   return accessToken?.token;
+// // }
 // import { decode } from "next-auth/jwt";
 // import { cookies } from "next/headers";
 
 // export async function getUserToken() {
-//   const x = (await cookies()).get("next-auth.session-token")?.value;
-//   const accessToken = await decode({ token: x, secret: process.env.NEXTAUTH_SECRET! });
+//   const x = (await cookies()).get("authjs.session-token")?.value;
+//   const accessToken = await decode({
+//     token: x,
+//     secret: process.env.NEXTAUTH_SECRET!,
+//     salt: "authjs.session-token",
+//   });
 //   return accessToken?.token;
 // }
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-  const x = (await cookies()).get("authjs.session-token")?.value;
+  const allCookies = await cookies();
+
+  // على Vercel بيكون اسمه مختلف
+  const cookieName = process.env.NODE_ENV === "production" ? "__Secure-authjs.session-token" : "authjs.session-token";
+
+  const x = allCookies.get(cookieName)?.value;
+
   const accessToken = await decode({
     token: x,
     secret: process.env.NEXTAUTH_SECRET!,
-    salt: "authjs.session-token",
+    salt: cookieName,
   });
+
   return accessToken?.token;
 }
